66940d30f54fb2f64457c16e3c25e5f9826de4d9
PraxTube <lukinator28@gmail.com>
2023-02-28 18:41:56 +0100

    Add header input data function
    Currently we can add the header data to all sheets. Next step is to
    start the random hour generation.

--- test.py
+++ test.py
@@ -1,8 +1,36 @@
+import time
 from openpyxl.reader.excel import load_workbook
 
-wb=load_workbook("zeitbogen.xlsx")
-ws = wb["Januar 23"]
-ws.cell(row=1, column=1).value= 'HARABARAM'
-# save the workbook to a new file to finish the editing
-# but the style settings has been removed (such like font, color) in the new file
-wb.save("out.xlsx")
+
+#######
+# CONSTS
+######
+
+input_file = "zeitbogen.xlsx"
+output_file = "output.xlsx"
+
+name = "Klausius Mausius"
+hours_each_week = 11
+days_each_week = 5
+department = "CLOWN SCHOOL"
+
+
+def input_header_data(sheet):
+    sheet.cell(row=3, column=5).value = name
+    sheet.cell(row=3, column=10).value = department
+    sheet.cell(row=4, column=2).value = hours_each_week
+    sheet.cell(row=4, column=5).value = days_each_week
+    sheet.cell(row=4, column=7).value = time.strftime("%H:%M:%S", time.gmtime(3600 * hours_each_week / days_each_week)) 
+    print(sheet.cell(4, 7).value)
+
+
+def main():
+    wb = load_workbook(input_file)
+    for sheet in wb.worksheets:
+        input_header_data(sheet)
+    wb.save(output_file)
+
+
+if __name__ == "__main__":
+    main()
+
